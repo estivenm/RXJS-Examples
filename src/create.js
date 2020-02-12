cc5b68339bd8481d30405b2c52b1f9fb93f3bf9e
jemazo <jemazo@bancolombia.com.co>
2020-02-12 10:12:12 -0500

    Implementa funcion interval y timer de rxjs

--- src/create.js
+++ src/create.js
@@ -1,9 +1,27 @@
 import { displayLog } from './utils';
-
+import  { interval, timer } from 'rxjs'
 export default () => {
+
     /** start coding */
+	const sourceInterval = interval(500);
+	const subscriptionInterval = sourceInterval.subscribe(data => displayLog(data));
+	
+	timer(3000).subscribe(() => subscriptionInterval.unsubscribe());
+	
+	// Se espera que se ejecute despues de  4seg cada 100 milisegundos
+	const sourceTimer = timer(4000, 100);
+	const subscriptionTimer = sourceTimer.subscribe(data => displayLog(`Timer 2 - ${data}`));
+	
+	// Eliminar subscripcion del timer  despues de  6 segundos
+	timer(6000).subscribe(() => subscriptionTimer.unsubscribe());
+	
+	/**
+	 * NOTA:
+	 * Funcion Interval permite crear secuecias de observables cada determinado tiempo
+	 * se debe controlar la finalizacion de la ejecucion del interval 
+	
+	 * Timer obterner un evento cadadeterminado tiempo
 
-
-
+	 */
     /** end coding */
 }
