64d33c170b852653f357826a40abf3f7e575e162
jemazo <jemazo@bancolombia.com.co>
2020-02-12 09:15:04 -0500

    implementar metodo from en array, ademas se convierte una promesa en un  observable

--- src/create.js
+++ src/create.js
@@ -1,7 +1,32 @@
 import { displayLog } from './utils';
-
+import Observable, { from } from 'rxjs';
 export default () => {
     /** start coding */
 
-    /** end coding */
+	const myArray = [1,2,3,4,5,6]
+	const myString =  "Hello word";
+
+	const myPromise = new Promise(resolve => setTimeout(() => {
+		resolve('Hello word')
+	}, 2000));
+
+	
+	const observaleMyArray = from(myArray);
+	const subscriptionMyArray = observaleMyArray.subscribe(val => displayLog(val));
+
+	const observaleMyString = from(myString);
+	const subscriptionMyString = observaleMyString.subscribe(val => displayLog(val));
+	
+	// Conservertir promesa en Observable
+
+	const observableMyPromise = from(myPromise);
+	const subscriptionMyPromise = observableMyPromise.subscribe(val => displayLog(val))
+	
+	/** end coding */
+
+	/** 
+	 *Nota las promesas y observables permiten devolver eventos asincronos,
+	 las promesas solo permiten devolver un evento, los observables un flujo de datos completos
+	 ademas estas se pueden cancelar cuando se desee
+	 */
 }
