3012aa2ba1496fab1866d159efce456870aaa3ae
jemazo <jemazo@bancolombia.com.co>
2020-02-13 07:42:06 -0500

    Implementa  fromEvent

--- src/create.js
+++ src/create.js
@@ -1,9 +1,28 @@
 import { displayLog } from './utils';
 
+import { fromEvent } from 'rxjs';
+
 export default () => {
-    /** start coding */
+	/** start coding */
+
+	// crear referencia  para el boton
+	const actionBtn = document.getElementById('action-btn');
+
+	//Generar un Observable apartir del evento click
+	const sourceClickBtn =  fromEvent(actionBtn, 'click');
+
+	sourceClickBtn.subscribe(evn => displayLog(`click event at pos ${evn.x}, ${evn.y}`));
+
+	fromEvent(document, 'mousemove')
+	.subscribe(even => {
+		console.log(even);
+	});
 
 
+	/**
+	 * Nota
+	 * fromEvent asocia una funcion o accion concreta a un observable
+	 */
 
     /** end coding */
 }
