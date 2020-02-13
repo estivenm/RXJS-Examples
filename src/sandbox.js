8c89b419a57f1a2bd4bd87117581deeaf5a3fd20
jemazo <jemazo@bancolombia.com.co>
2020-02-13 08:33:42 -0500

    Implementa operator tap

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,18 +1,26 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
     const click$ = fromEvent(grid, 'click').pipe(
+        tap( data => console.log('Before:', data )),
         map(val => [ 
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
-        ])
+        ]),
+        tap( data => console.log('After:', data ))
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
 
+    /**
+     * Nota operador tap
+     * permite leer infomacion en cualquier punto del stream de datos,
+     * no permite mnodificar el stream de datos
+     * 
+     */
     /** end coding */
 }
