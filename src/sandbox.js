e645fdca90e520dcb9d040520df25b8fb3699fc3
jemazo <jemazo@bancolombia.com.co>
2020-02-14 10:50:28 -0500

    Implementa operador reducer y scan

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, takeWhile, tap } from 'rxjs/operators';
+import { map, takeWhile, tap, reduce, scan } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -12,9 +12,18 @@ export default () => {
         ]),
         takeWhile( ([col, row]) => col != 0 ),
         tap(val => console.log(`cell: [${val}]`)),
+        scan((accumalated, current)=> { 
+             return {
+                 clicks: accumalated.clicks + 1,
+                 cells: [ ...accumalated.cells, current]
+             }
+        },{clicks:0, cells:[]})
     );
 
-    const subscription = click$.subscribe(data => displayLog(data));
+    const subscription = click$.subscribe(data => displayLog(`${data.clicks}, clicks:${JSON.stringify(data.cells)}` ));
 
+    /** Reduce aplica una misma funcion a acada evento que pasa por el stream
+        y devuelve el resultado cuando finaliza 
+    **/
     /** end coding */
 }
