c8ea349b0d02cbcf5e367f0f5e9d421ecb18a4b0
jemazo <jemazo@bancolombia.com.co>
2020-02-14 07:54:42 -0500

    Implementa operador last, takeLast, skip

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, takeWhile } from 'rxjs/operators';
+import { map, takeWhile, tap, last } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -10,10 +10,16 @@ export default () => {
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
         ]),
-        takeWhile( ([col, row]) => col > 3 )
+        takeWhile( ([col, row]) => col > 3 ),
+        tap(val => console.log('valid in takeWhile', val)),
+        last()
+        // takeLast(3),
+        // skip(5)
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
-
+        // last  emite  ultimo valor que ha recibido
+        // takeLast  emite los ultimos x cantidad de valores recibidos
+        // Skip permite obmitir una x  cantidad de valores 
     /** end coding */
 }
