afa413d3074920a32fe5a2e6b7fb1e34a978c4bf
jemazo <jemazo@bancolombia.com.co>
2020-02-17 09:28:04 -0500

    Implementa Subject y BehaviorSubject

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,5 +1,5 @@
 import { updateDisplay } from './utils';
-import { fromEvent } from 'rxjs';
+import { fromEvent, Subject, BehaviorSubject } from 'rxjs';
 import { map, tap, share } from 'rxjs/operators';
 
 export default () => {
@@ -28,13 +28,29 @@ export default () => {
         share()
     )
 
+    // const  scrollProgressHot$ =  new  Subject();
+    const  scrollProgressHot$ =  new  BehaviorSubject(0);
+
+    scrollProgress$.subscribe(scrollProgressHot$);
+
     //subscribe to scroll progress to paint a progress bar
-    const subscription = scrollProgress$.subscribe(updateProgressBar);
+    const subscription = scrollProgressHot$.subscribe(updateProgressBar);
 
     //subscribe to display scroll progress percentage
-    const subscription2 = scrollProgress$.subscribe(
+    const subscription2 = scrollProgressHot$.subscribe(
         val => updateDisplay(`${ Math.floor(val) } %`)
     );
 
+    // scrollProgressHot$.next(0)
+    console.log('valor inicial de scrollProgressHot:', scrollProgressHot$.value)
+
+    /**
+     * Subject  tipo especial de Observable (permite multi casth de eventos para varios observadores)
+     * Distribuidor de eventos , ademas permite emitir sus propios eventos 
+     * 
+     * BehaviorSubject
+     * Actua como Subject pero este guarda el ultimo evento emitido (se puede acceder a este en cualquier momento)
+     * se inicializa con un valor por defecto
+     */
     /** end coding */
 }
