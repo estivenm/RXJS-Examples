415140f8555311323ecbe3017bde16545ce41b54
jemazo <jemazo@bancolombia.com.co>
2020-02-26 10:44:57 -0500

    implementa operadores temporalesbufferTime, delay

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { updateDisplay } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, delay, bufferTime } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -17,19 +17,26 @@ export default () => {
     const scroll$ = fromEvent(document, 'scroll').pipe(
         map(() => docElement.scrollTop),
         tap(evt => console.log("[scroll]: ", evt))
-    );
-
-    //observable that returns the amount of page scroll progress
-    const scrollProgress$ = scroll$.pipe(
-        map(evt => {
-            const docHeight = docElement.scrollHeight - docElement.clientHeight;
-            return (evt / docHeight) * 100;
-        })
+        );
+        
+        //observable that returns the amount of page scroll progress
+        const scrollProgress$ = scroll$.pipe(
+            map(evt => {
+                const docHeight = docElement.scrollHeight - docElement.clientHeight;
+                return (evt / docHeight) * 100;
+            }),
+            bufferTime(2000,1000),
+            tap(evt => console.log("[buffer]: ", evt))
     )
 
     //subscribe to scroll progress to paint a progress bar
     const subscription = scrollProgress$.subscribe(updateProgressBar);
 
 
+    /**
+     *  delay  este operador  permite generar un retraso en el flujo de datos
+     * bufferTime se ejecuta cada determinado tiempo y guarda un flujo de muestra en ese interavalo de tiempo 
+     * (tambien se puede eapecificar en que tiempo emitir esas muestras recolectadas )
+     */
     /** end coding */
 }
