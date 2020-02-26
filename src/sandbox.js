90d77fd72b4785bd1c5f78b080213accc345cdf7
jemazo <jemazo@bancolombia.com.co>
2020-02-26 10:31:15 -0500

    implementa operadores temporales throttleTime, sampleTime y auditTime

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { updateDisplay } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, sampleTime, auditTime, throttleTime } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -15,6 +15,9 @@ export default () => {
 
     //observable that returns scroll (from top) on scroll events
     const scroll$ = fromEvent(document, 'scroll').pipe(
+        sampleTime(50),
+        auditTime(50),
+        throttleTime(),
         map(() => docElement.scrollTop),
         tap(evt => console.log("[scroll]: ", evt))
     );
@@ -30,6 +33,12 @@ export default () => {
     //subscribe to scroll progress to paint a progress bar
     const subscription = scrollProgress$.subscribe(updateProgressBar);
 
-
+    /**
+    * operadores temporares (limitar frecuencia de emitir eventos)
+    * sampleTime emite el valor mas reciente dentro de un flujo de datos cada  determinado tiempo
+    * auditTime espera a detectar un evento, luego espera  determinado retraso para ejecutarse
+    *  throttleTime detecta un evento y lo emite, luego espera  determinado tiempo para ejecutarse nuevamente 
+    * (no garariza obtener el ultimo valor ejecutado)
+    */
     /** end coding */
 }
