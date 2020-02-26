47e58bd7eaa12522ac60e1044af372cce8e73d7e
jemazo <jemazo@bancolombia.com.co>
2020-02-26 15:48:47 -0500

    implementa operadores merge, zip

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { updateDisplay, displayLog } from './utils';
-import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { fromEvent, merge, zip } from 'rxjs';
+import { map, tap, scan, filter, distinctUntilChanged } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -22,7 +22,7 @@ export default () => {
     }
 
     /** helper method to retrieve local coords from click */
-    const getLocalClickCoords = (event, parent) =>{
+    const getLocalClickCoords = (event, parent) => {
         return {
             x: event.clientX - parent.offsetLeft,
             y: event.clientY - parent.offsetTop,
@@ -54,12 +54,51 @@ export default () => {
                 label: 'drawing',
                 coords: getLocalClickCoords(event, canvas)
             }
-        }));        
+        }));
 
 
     //TODO: draw current line
+    // const drawLine$ = zip(mouseStart$, mouseEnd$).pipe(
+    //     tap(console.log),
+    //     map(([start, end]) => {
+    //         return {
+    //             origin: start.coords,
+    //             end: end.coords
+    //         }
+    //     })
+    // );
+    // const drawLine$ = merge(mouseStart$,mouseMove$, mouseEnd$).pipe(
+    //     tap(console.log)
+    // );
+    // drawLine$.subscribe(data => drawLine(data.origin, data.end));
 
-    
+    const computedDrawState = (prevState, event) => {
+        switch (prevState.label) {
+            case 'init':
+            case 'end':
+                if (event.label == 'start') {
+                    return { origin: event.coords, ...event }
+                }
+                break;
+            case 'start':
+            case 'drawing':
+                return { origin: prevState.origin, ...event }
+        }
+        return prevState;
+    };
 
+    const drawLine$ = merge(mouseStart$, mouseMove$, mouseEnd$).pipe(
+        tap(console.log),
+        scan(computedDrawState, { label: 'init' }),
+        filter(data => data.origin && data.coords),
+        // distinctUntilChanged(),
+        tap(console.log)
+    )
+    drawLine$.subscribe(data => drawLine(data.origin, data.coords));
+    /**
+     * Zip combina varios flujos de datos en flujo de datos por order
+     * Merge entrega los datos en un unico flujo de datos
+     * distinctUntilChanged no permite emitir eventos repetidos   
+     */
     /** end coding */
 }
