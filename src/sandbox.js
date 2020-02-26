9b905d6493c3d441b6cd88f40b942a79f028d89e
jemazo <jemazo@bancolombia.com.co>
2020-02-26 11:05:33 -0500

    implementa operadores temporal debouceTime

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,20 @@
 import { updateDisplay, displayLog } from './utils';
+import { fromEvent } from 'rxjs';
+import { map, debounceTime } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     
     const inputBox = document.getElementById('input-box');
+    const inputSrc = fromEvent(inputBox, 'input').
+    pipe(
+        debounceTime(300),
+        map(event => event.target.value),
+    );
+    inputSrc.subscribe(displayLog)
 
+    /** 
+     *  debounceTime  espera un perido de guarda antes emiter el evento mas reciente
+    */
     /** end coding */
 }
