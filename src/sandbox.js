5b1c15a855e12e8462cd07ac31a6678dd78fb051
jemazo <jemazo@bancolombia.com.co>
2020-03-09 15:59:50 -0500

    agrega operador materialize y dematerialize

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { updateDisplay } from './utils';
 import { fromEvent, interval, merge, EMPTY } from 'rxjs';
-import { mapTo, scan, takeWhile, switchMap, startWith  } from 'rxjs/operators';
+import { mapTo, scan, takeWhile, switchMap, startWith, materialize, tap, dematerialize  } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -23,13 +23,20 @@ export default () => {
     /** countdown timer */
     const countdown$ = isPaused$.pipe(
         startWith(false),
-        switchMap(paused => !paused ? interval$ : EMPTY),
+        switchMap(paused => !paused ? interval$.pipe(materialize()) : EMPTY.pipe(materialize())),
+        dematerialize(),
         scan((acc, curr) => ( curr ? curr + acc : curr ), countdownSeconds),
-        takeWhile(v => v >= 0)
+        takeWhile(v => v >= 0),
+        tap(console.log)
     );
 
+    /**
+     * Noticiaciones
+     * Objeto que representa un accion push d eun observable
+     * Materialize permite emitir las acciones como eventos de metadatos
+     */
     /** subscribe to countdown */
-    countdown$.subscribe(updateDisplay);
+    countdown$.subscribe(updateDisplay, null, () => console.log('completed'));
 
     
     /** end coding */
