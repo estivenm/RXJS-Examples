d644ce5d4ca46d40633b78cb02e729039aa9592b
jemazo <jemazo@bancolombia.com.co>
2020-05-14 14:42:22 -0500

    Creacion observable con interval

--- src/create.js
+++ src/create.js
@@ -1,35 +1,17 @@
 import { displayLog } from './utils';
-import { Observable } from 'rxjs';
+import { interval } from 'rxjs';
 
 export default () => {
+
 	/** start coding */
-	const hello = Observable.create(subscriber => {
+	const observable = interval(1000);
 
-		subscriber.next('hello');
-		setTimeout(() => {
-			subscriber.next('word');
-			// terminar la subscripcion
-			subscriber.complete();
-		}, 2000);
-	});
+	const subscription = observable.subscribe( (x) => { console.log(x);  displayLog(x)} );
 
-	// Estructura de un observer
-	const observer = {
-		next: env => displayLog(env),
-		error: err => console.error('[Error]: ',err),
-		complete: () => displayLog('[DONE]')
+	setTimeout(() => {
+			subscription.unsubscribe();
+	},4000) 
 
-	}
-	
-	//Ejecutar observable
-	//const subscribe = hello.subscribe(env => displayLog(env));
-	
-	//Ejecutar obserbable con estructura predefinida
-	const subscribe1 = hello.subscribe(observer);
-	
-	// cancelar susbcricion fuera del observable
-	subscribe1.unsubcribe();  
-
-	// NOTA: Cada ejecuacion de un observable  implica una nueva subscripcion mde todo el flujo de datos
+	// NOTA: Cada ejecuacion de un observable implica una nueva subscripcion mde todo el flujo de datos
 	/** end coding */
 }
